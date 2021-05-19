66dfc99d75c5b152b7109f18c4b6531c37d7daa7
Tuyen Pham <tuh39709@temple.edu>
2021-05-19 15:44:51 -0400

    user register then store to mongodb

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,6 +1,29 @@
+const Users = require('../models/userModel')
+const bcrypt = require('bcrypt')
+
 const userCtrl = {
-    register: (req,res) => {
-        res.json({msg: 'Test Controllers'})
+    register: async(req,res) => {
+        try {
+            const { name, email, password } = req.body
+            const user = await Users.findOne({ email })
+
+            if (user) return res.status(400).json({msg: 'The email already exists'})
+            if (password.length < 6)
+                return res.status(400).json({msg: 'Password is at least 6 characters long'})
+            
+            // Password Encryption
+            const passwordHash = await bcrypt.hash(password, 10)
+            const newUser = new Users({
+                name, email, password: passwordHash
+            })
+            
+            // Save Mongodb
+            await newUser.save()
+            res.json({msg: 'Register success'})
+
+        } catch (err){
+            return res.status(500).json({msg: err.message})
+        }
     }
 }
 

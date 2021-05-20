cb43fa4540953a092de928db4e47d50aa6165483
Tuyen Pham <tuh39709@temple.edu>
2021-05-20 11:36:52 -0400

    refresh token for user

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const Users = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userCtrl = {
     register: async(req,res) => {
@@ -19,12 +20,46 @@ const userCtrl = {
             
             // Save Mongodb
             await newUser.save()
-            res.json({msg: 'Register success'})
+            // then create jsonwebtoken to authentication
+            const accesstoken = createAccessToken({id: newUser._id})
+            const refreshtoken = createRefreshToken({id: newUser._id})
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+
+            res.json({accesstoken})
+            // res.json({msg: 'Register success'})
 
         } catch (err){
             return res.status(500).json({msg: err.message})
         }
+    },
+    refreshToken: (req,res) => {
+        try {
+            const rf_token = req.cookies.refreshtoken
+            if (!rf_token) return res.status(400).json({msg: 'Please login or register!'})
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) return res.status(400).json({msg: 'Please login or register!'})
+                const accesstoken = createAccessToken({id: user.id})
+                res.json({user, accesstoken})
+            })
+
+            // res.json({ rf_token })
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
+}
+
+const createAccessToken = (user) => {
+    return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
+
+const createRefreshToken = (user) => {
+    return jwt.sign( user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
 }
 
 module.exports = userCtrl

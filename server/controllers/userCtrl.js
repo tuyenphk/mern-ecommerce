c4845824a1b7d1401a348a9676580513b4e07558
Tuyen Pham <tuh39709@temple.edu>
2021-05-20 13:53:47 -0400

    user authorization

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -85,6 +85,16 @@ const userCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+            if (!user) return res.status(400).json({msg: 'User does not exist'})
+
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
 }
 

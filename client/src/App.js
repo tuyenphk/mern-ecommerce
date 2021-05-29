84e5f9f1a5f325f51e4fa37997ed318050489086
Tuyen Pham <tuh39709@temple.edu>
2021-05-28 21:53:34 -0400

    setup header client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {DataProvider} from './GlobalState'
+import Header from './components/Header/Header'
+import MainPage from './components/Pages/MainPage'
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <DataProvider>
+      <Router>
+        <div className="app">
+          <Header />
+          <MainPage />
+        </div>
+      </Router>
+    </DataProvider>
   );
 }
 
---
+++ client/src/components/Pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainPage() {
+    return (
+        <div>
+           Main Pages Component 
+        </div>
+    )
+}
+
+export default MainPage

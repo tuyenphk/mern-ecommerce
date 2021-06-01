8867a909a5a8cafafd496ccda0aa784cdd88ac81
Tuyen Pham <tuh39709@temple.edu>
2021-06-01 14:19:35 -0400

    setup client side components

--- client/src/components/Pages/MainPage.js
+++ client/src/components/Pages/MainPage.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/NotFound/NotFound'
+import { Switch, Route } from 'react-router-dom'
 
 function MainPage() {
     return (
-        <div>
-           Main Pages Component 
-        </div>
+        <Switch>
+            <Route path="/" exact component={Products} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/cart" exact component={Cart} />
+
+            <Route path="*" exact component={NotFound} />
+        </Switch>
     )
 }
 

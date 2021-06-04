233704192eb679e26b2a1ae953e9909a1f45ba84
Tuyen Pham <tuh39709@temple.edu>
2021-06-04 13:33:25 -0400

    display products on frontend

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,10 +1,18 @@
 import React, {createContext, useState} from 'react'
+import ProductsAPI from './api/ProductsAPI'
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) => {
+    const [token, setToken] = useState(false)
+
+    const state = {
+        token: [token, setToken],
+        productsAPI: ProductsAPI()
+    }
+    
     return (
-        <GlobalState.Provider value={"Value in Global"}>
+        <GlobalState.Provider value={state}>
             {children}
         </GlobalState.Provider>
     )
--- client/src/components/Pages/products/Products.js
+++ client/src/components/Pages/products/Products.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import ProductItem from './ProductItem'
 
 function Products() {
+    const state = useContext(GlobalState)
+    const [products] = state.productsAPI.products
+    console.log(products)
+
     return (
-        <div>
-            Products List
+        <div className="products">
+            {
+                products.map(product => {
+                    return <ProductItem key={product._id} product={product} />
+                })
+            }
         </div>
     )
 }

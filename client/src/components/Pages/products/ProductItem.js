e731c7ed36df0d551b16a2ed0035b90ca13da796
Tuyen Pham <tuh39709@temple.edu>
2021-06-04 16:02:27 -0400

    add style of products item

--- client/src/components/Pages/products/ProductItem.js
+++ client/src/components/Pages/products/ProductItem.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function ProductItem({product}) {
     return (
         <div className="product_card">
-            <img src={product.images.ul} alt="" />
+            <img src={product.images.url} alt="" />
 
             <div className="product_box">
                 <h2 title={product.title}>{product.title}</h2>
                 <span>${product.price}</span>
                 <p>{product.description}</p>
+            </div>
+
+            <div className="row_btn">
+                <Link id="btn_buy" to="#!">Buy</Link>
+                <Link id="btn_view" to={`detail/${product._id}`}>View</Link>
             </div>
         </div>
     )
